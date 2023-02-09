441889f5f844688263bbc6ebed781614cdb121b4
BhuvanVadlapudi <124895531+BhuvanVadlapudi@users.noreply.github.com>
2023-02-09 19:39:37 +0530

    Updated the code  in gcd.py

--- gcd.py
+++ gcd.py
@@ -2,6 +2,13 @@ num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 
 # greatest common divisor
-result = num1 + num2
+if(num1>num2):
+  n=num1
+else:
+  n=num2
+result=0;
+for i in range(n+1):
+  if(num1%i==0 and num2%i==0):
+    result=i
 
 print("The GCD of", num1, "and", num2, "is", result)
